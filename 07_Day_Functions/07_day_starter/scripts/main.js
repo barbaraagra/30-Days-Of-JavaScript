4399750b40f93263111547e84137e3e2e84f2a8b
barbaraagra <babs.agra@gmail.com>
2023-06-21 09:20:32 +0100

    exercises day 6.2

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -13,3 +13,19 @@ function addNumbers(numOne, numTwo) {
     return numOne + numTwo;
 }
 console.log(addNumbers(1, 2));
+
+//An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+function areaOfRectangle(length, width) {
+    return length * width;
+}
+console.log(areaOfRectangle(10, 5))
+
+//A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+function perimeterOfRectangle(len, wid) {
+    return (len + wid) * 2
+}
+console.log(perimeterOfRectangle(1, 2));
+
+//A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+const volumeOfRectPrism = (leng, widt, heig) => leng * widt * heig;
+console.log(volumeOfRectPrism(4, 2, 3));
