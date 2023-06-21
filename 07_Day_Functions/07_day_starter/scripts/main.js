d48cbae631e0716560080620d631b77ee8355a22
barbaraagra <babs.agra@gmail.com>
2023-06-21 10:02:46 +0100

    day6 javascript

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -37,3 +37,21 @@ console.log(areaOfCircle(2));
 //Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
 const circumOfCircle = (r, π = 3.14) => 2 * π * r;
 console.log(circumOfCircle(6));
+
+//Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+const densityOfSub = (mass, vol) => mass / vol;
+console.log(densityOfSub(10, 20))
+
+//Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+const speedObj = (dist, time) => dist / time;
+console.log(`${speedObj(400, 10)}km/h`);
+
+//Weight of a substance is calculated as follows: weight = mass x gravity. Write a function which calculates weight.
+const calWeigth = (mass, gravity = 9.81) => mass * gravity;
+console.log(calWeigth(100));
+
+//Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
+const celToFar = (celsius) => (celsius * 9 / 5) + 32;
+console.log(celToFar(40));
+
+//Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
